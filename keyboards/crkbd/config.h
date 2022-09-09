4cdd3ab158e9374292fa8354d57683a6b3ca2709
tr1sk <112913114+tr1sk@users.noreply.github.com>
2022-09-09 13:48:05 -0700

    Update config.h

--- keyboards/crkbd/config.h
+++ keyboards/crkbd/config.h
@@ -41,6 +41,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
 
+#define USB_POLLING_INTERVAL_MS 1
+#define QMK_KEYS_PER_SCAN 12
+
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 //#define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
